d1480358d4de9c6e64c7d468093a8fe0d1bc0cca
DerTabii <62552121+DerTabii@users.noreply.github.com>
2020-04-04 14:38:18 +0200

    add island

--- VektoriaApp/Game.h
+++ VektoriaApp/Game.h
@@ -24,12 +24,63 @@
 
 using namespace Vektoria;
 
+#define TERRAIN_SIZE 1000
+#define TERRAIN_VERTICES 2000
+#define TERRAIN_HEIGHT 350
+#define TERRAIN_SAND_MAX 12
+#define TERRAIN_GRASS_MAX 150
+#define TERRAIN_SNOW_MAX 300
+
 
 class CGame
 {
 public:
 	CGame(void);																				// Wird vor Begin einmal aufgerufen (Konstruktor)
-	~CGame(void);																				// Wird nach Ende einmal aufgerufen (Destruktor)
+	~CGame(void);	// Wird nach Ende einmal aufgerufen (Destruktor)
+
+	CRoot m_root;
+	CScene m_scene;
+
+	CLightParallel m_light;
+	CGeoSphere m_Kugel;
+	CPlacement m_pkugel;
+
+	CFrame m_frame;
+	CViewport m_viewport;
+	CCamera m_camera;
+	CDeviceKeyboard m_keyboard;
+	CPlacement m_pCamera;
+
+	CGeoTerrain m_gTerrainOri; //Gesamtes Terrain
+	CGeoTerrain m_gTerrain; //inselausschnitt
+	CGeoTerrain m_gTerrainSand; //Sandausschnitt
+	CGeoTerrain m_gTerrainSandMossy;
+	CGeoTerrain m_gWater; //wasserausschnitt
+	CGeoTerrain m_gTerrainGrass; //Grasausschnitt
+
+	CPlacement m_pIsland1; //insel- wasserplacement
+
+	CPerlin * m_pperlin = nullptr; //Perlin-Noise
+
+	CBlob * m_pblob1 = nullptr; //Erhebung aus dem meer
+	CBlob* m_pblob_rest = nullptr; //Artefaktlöschung
+
+	CCut m_cutUnderSea; //Schnitt unter N.N.
+	CCut m_cutInverseBeachToSnow;
+	CCut m_cutInverseBeach; //strandschnitt
+	CCut m_cutInverseGrass;
+	CCut m_cutInverseSnow;
+
+	CCut m_cutInverseUnder15Degrees;
+	CCut m_cutUnder15Degrees;
+
+	CMaterial m_mWater;	//Wellenmaterial
+	CMaterial m_mSand; //Strandmaterial
+	CMaterial m_mSandMossy;
+	CMaterial m_mEarthMossy; //Erdmaterial
+
+	CGeos m_gsCol, m_gsHeight; //Kollisionscontainer
+	CGeoTerrains m_gsTerrain; //Terrainkollisionscontainer
 
 	void Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash * psplash);	// Wird zu Begin einmal aufgerufen
 	void Tick(float fTime, float fTimeDelta);													// Wird während der Laufzeit bei jedem Bildaufbau aufgerufen
