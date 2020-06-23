cb621ee9c8595a3ba6f5fcde679b461424bdf22b
Adrian <adihuber12@gmail.com>
2020-06-23 02:02:32 +0200

    random plant seeds

--- VektoriaApp/Forest/Forest.h
+++ VektoriaApp/Forest/Forest.h
@@ -5,10 +5,7 @@
 #include "Geos/Nadelbaum.h"
 #include "ForestCluster.h"
 #include "PlantPlacement.h"
-
-#define CBTREE_SEED 30285
-#define CONIFER_SEED 29486
-#define POPPY_SEED 44444
+#include <time.h>
 
 using namespace Vektoria;
 
@@ -38,9 +35,9 @@ namespace ForestNS
         }
 
     private:
-        PlantPlacement<CherryBlossomTree, 4> m_zpCherryBlossomTree = PlantPlacement<CherryBlossomTree, 4>(CBTREE_SEED, 150.0f, 0.2f, 0.0f);
-        PlantPlacement<GeoBioPoppy, 3> m_zpPoppy = PlantPlacement<GeoBioPoppy, 3>(POPPY_SEED, 1.0f, 0.2f, 0.0f);
-        PlantPlacement<Nadelbaum, 4> m_zpConifer = PlantPlacement<Nadelbaum, 4>(CONIFER_SEED, 300.0f, 0.2f, 0.0f);
+        PlantPlacement<CherryBlossomTree, 4> m_zpCherryBlossomTree = PlantPlacement<CherryBlossomTree, 4>(time(NULL), 150.0f, 0.2f, 0.0f);
+        PlantPlacement<GeoBioPoppy, 3> m_zpPoppy = PlantPlacement<GeoBioPoppy, 3>(time(NULL), 1.0f, 0.2f, 0.0f);
+        PlantPlacement<Nadelbaum, 4> m_zpConifer = PlantPlacement<Nadelbaum, 4>(time(NULL), 300.0f, 0.2f, 0.0f);
 
         std::vector<ForestCluster*> m_forestClusters;
         std::vector<CPlacement*> m_zpFlowers;
